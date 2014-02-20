485fc23830437244a953a61baa21c879f1b010bd
Jon Bevan <jon@jdbevan.com>
2014-02-20 00:00:58 +0000

    Default URL const

--- consts.inc.php
+++ consts.inc.php
@@ -4,6 +4,8 @@ define("USER_AGENT_GOOGLE_BOT", "Googlebot/2.1 (+http://www.google.com/bot.html)
 define("USER_AGENT_SAFARI_IPAD", "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405");
 define("USER_AGENT_CHROME_LINUX", "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/31.0.1650.63 Chrome/31.0.1650.63 Safari/537.36");
 
+define('DEFAULT_URL',           'http://www.example.com/foo.php?bar=baz');
+
 define("COND_COMPARE_INT_EQ",	1);
 define("COND_COMPARE_INT_GTE",	2);
 define("COND_COMPARE_INT_GT",	3);
